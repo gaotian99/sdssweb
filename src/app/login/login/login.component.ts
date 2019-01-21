099cc91feb947bc6c55eef204a4124deb00f80b0
gaotian99 <gaotian99@hotmail.com>
2019-01-21 00:08:14 -0800

    change random article to signin page

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,8 +1,9 @@
 //import { Component, OnInit } from '@angular/core';
 
-import {HttpClient} from '@angular/common/http';
-import {Component, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of as ObservableOf } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
@@ -16,24 +17,44 @@ export class LoginComponent implements OnInit {
   oneContent: string = "Please wait";
   onePic: string = "";
   oneUrl: string = "http://www.espn.com";
+  getNum: number = 0;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.dataDatabase = new NewsApiHttpDao(this.http);
-    this.dataDatabase.getNewsFromHttp().toPromise()
-      .then( datab => {
-        this.oneTitle = datab.articles[0].title;
-        this.oneContent = datab.articles[0].description;
-        this.onePic = datab.articles[0].urlToImage;
-        this.oneUrl = datab.articles[0].url;
-      } ).catch(this.handleError);
+    this.dataDatabase!.getNewsFromHttp().pipe(
+      map(data => {
+        this.getNum = (data.totalResults !== null) ? data.totalResults : 0;
+        //if(this.getNum > 0) this.getNum = Math.floor(Math.random()*this.getNum);
+        return data.articles;
+      }),
+      catchError(() => ObservableOf([]))
+    ).subscribe(data => {
+      if (this.getNum > 0) {
+        let pickNum: number = Math.floor(Math.random()*this.getNum);
+        this.oneTitle = data[pickNum].title;
+        this.oneContent = data[pickNum].description;
+        this.onePic = data[pickNum].urlToImage;
+        this.oneUrl = data[pickNum].url;
+      }
+    });
+
+
+
+    //.toPromise()
+    //  .then( datab => {
+    //    this.oneTitle = datab.articles[0].title;
+    //    this.oneContent = datab.articles[0].description;
+    //    this.onePic = datab.articles[0].urlToImage;
+    //    this.oneUrl = datab.articles[0].url;
+    //  } ).catch(this.handleError);
   }
 
-  private handleError(error: any): Promise<any> {
-    console.error("error occurred", error);
-    return Promise.reject(error.message || error);
-  }
+  //  private handleError(error: any): Promise<any> {
+  //    console.error("error occurred", error);
+  //    return Promise.reject(error.message || error);
+  //  }
 
 }
 
@@ -51,7 +72,7 @@ export interface NewsArticle {
 }
 
 export class NewsApiHttpDao {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getNewsFromHttp(): Observable<NewsApi> {
     const requestUrl = "https://newsapi.org/v2/top-headlines?sources=espn&apiKey=e00ae47e21d5480fba47277a55348d0d";
